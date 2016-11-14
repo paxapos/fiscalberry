2d94773c48b998ab58e28e102482ba18aabc527c
Ale Vilar <alevilar@gmail.com>
2016-11-14 14:14:20 -0300

    conector que conecta y libera el recurso

--- Drivers/ReceiptDirectJetDriver.py
+++ Drivers/ReceiptDirectJetDriver.py
@@ -14,29 +14,25 @@ class ReceiptDirectJetDriver( printer.Network ):
 	def __init__(self, host, port=9100, timeout=10, codepage="cp858"):
 		""" escrito aqui solo para tener bien en claro las variables iniciales"""
 		"""
-        :param host : Printer's hostname or IP address
-        :param port : Port to write to
-        :param timeout : timeout in seconds for the socket-library
+		:param host : Printer's hostname or IP address
+		:param port : Port to write to
+		:param timeout : timeout in seconds for the socket-library
 		:param codepage : codepage default to cp858
-        """
-        Escpos.__init__(self, *args, **kwargs)
-        self.host = host
-        self.port = port
-        self.timeout = timeout
+		"""
+		Escpos.__init__(self, *args, **kwargs)
+		self.host = host
+		self.port = port
+		self.timeout = timeout
 
-    def start(self):
-    	try:
-			self.open()
-			self.connected = True
-		except Exception as e:
-			self.connected = False
+
+	def start(self):
+		""" iniciar """
+		self.open()
+		self.connected = True
 
 	def end(self):
-    	try:
-			self.close()
-			self.connected = False
-		except Exception as e:
-			self.connected = True
+		self.close()
+		self.connected = False
 
 
 	def reconnect(self):
