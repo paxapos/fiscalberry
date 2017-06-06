0117bde3d28bca74b3b62d07ac114ca8ae081a09
gonzaabel <gonzaloabel@hotmail.com>
2017-06-06 13:34:03 -0300

    Another commit

--- Comandos/HasarComandos.py
+++ Comandos/HasarComandos.py
@@ -282,15 +282,16 @@ class HasarComandos(ComandoInterface):
         return self._sendCommand(self.CMD_OPEN_FISCAL_RECEIPT, [type, "T"])
 
     def openBillCreditTicket(self, type, name, address, doc, docType, ivaType, reference="NC"):
-        reference = str(reference)
-        print("RETURN COMPROBANTE: ", self._sendCommand(self.CMD_CREDIT_NOTE_REFERENCE, ["1", reference]))
         print("ZARAZA: ", self._setCustomerData(name, address, doc, docType, ivaType))
         if type == "A":
             type = "R"
         else:
             type = "S"
+        reference = str(reference)
         self._currentDocument = self.CURRENT_DOC_CREDIT_BILL_TICKET
+        print("CANCEL: ", self.closeDocument())
         self._savedPayments = []
+        print("RETURN COMPROBANTE: ", self._sendCommand(self.CMD_CREDIT_NOTE_REFERENCE, ["1", reference]))
         print("RETURN OPEN NC: ", self._sendCommand(self.CMD_OPEN_CREDIT_NOTE, [type, "T", reference]))
 
     def openRemit(self, name, address, doc, docType, ivaType, copies=1):
