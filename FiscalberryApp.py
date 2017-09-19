5967a5f37234aa7b2b0a44919418c83f8c2fdc27
Ale Vilar <alevilar@gmail.com>
2017-09-18 22:52:17 -0300

    mejoras con generators de tornadi y mas

--- FiscalberryApp.py
+++ FiscalberryApp.py
@@ -1,3 +1,4 @@
+from tornado import gen
 import tornado.httpserver
 import tornado.websocket
 import tornado.ioloop
@@ -13,6 +14,7 @@ import ssl
 import Configberry
 import FiscalberryDiscover
 from  tornado import web
+import signal
 
 MAX_WAIT_SECONDS_BEFORE_SHUTDOWN = 2
 INTERVALO_IMPRESORA_WARNING = 30.0
@@ -24,7 +26,7 @@ clients = []
 
 root = os.path.dirname(__file__)
 
-traductor = TraductoresHandler()
+
 
 class WebSocketException(Exception):
 	pass
@@ -43,17 +45,24 @@ class PageHandler(tornado.web.RequestHandler):
 
 
 # inicializar intervalo para verificar que la impresora tenga papel
-#		timerPrinterWarnings = Timer(INTERVALO_IMPRESORA_WARNING, self.send_printer_warnings).start()
+#		
  
 class WSHandler(tornado.websocket.WebSocketHandler):
 
+	def initialize(self):
+		self.clients = []
+
+		self.traductor = TraductoresHandler()
+
+		tornado.ioloop.IOLoop.current().spawn_callback(self.send_printer_warnings, self.clients)
+
+
 	def open(self):
-		global clients
-		clients.append(self)
+		self.clients.append(self)
 		print 'new connection'
 	
 	def on_message(self, message):
-		global traductor
+		traductor = self.traductor
 		print("----- - -- - - - ---")
 		print message
 		try:
@@ -68,29 +77,27 @@ class WSHandler(tornado.websocket.WebSocketHandler):
 			response = {"err": repr(e)+"- "+str(e)}
 			import sys, traceback
 			traceback.print_exc(file=sys.stdout)
-
 	
-	def send_printer_warnings( self ):
-	    "enviar un broadcast a los clientes con los warnings de impresora, si existen"
-	    global clients
-	    global traductor
 
-	    warns = traductor.getWarnings()
+	def __procesarImpresoraEterna(self, clients):
+	    warns = self.traductor.getWarnings()
 	    if warns:
 			print warns
 	    	# envia broadcast a todos los clientes
 			msg = json.dumps( {"msg": warns } )
 			for cli in clients:
 				cli.write_message( msg )
-	    #volver a comprobar segun intervalo seleccionado  
-	    self.timerPrinterWarnings = Timer(INTERVALO_IMPRESORA_WARNING, self.send_printer_warnings)
-	    self.timerPrinterWarnings.start()
 
 
- 
+	@gen.coroutine
+	def send_printer_warnings( self, clients ):
+		while True:
+			yield self.__procesarImpresoraEterna(clients)
+			yield gen.sleep(INTERVALO_IMPRESORA_WARNING)
+
+
 	def on_close(self):
-		global clients
-		clients.remove(self)
+		self.clients.remove(self)
 		print 'connection closed'
  
 	def check_origin(self, origin):
@@ -99,7 +106,7 @@ class WSHandler(tornado.websocket.WebSocketHandler):
 
 
 
-class FiscalberryServer:
+class FiscalberryApp:
 	application = None
 	http_server = None
 
@@ -109,7 +116,43 @@ class FiscalberryServer:
 
 	def __init__(self):
 		print("Iniciando Fiscalberry Server")
+
+		def sig_handler(sig, frame):
+			logging.info('Caught signal: %s', sig)
+			tornado.ioloop.IOLoop.instance().add_callback(self.shutdown)
+
+		newpath = os.path.dirname(os.path.realpath(__file__))
+		os.chdir(newpath)		
+		signal.signal(signal.SIGTERM, sig_handler)
+		signal.signal(signal.SIGINT, sig_handler)
+
 		
+		self.start()
+
+
+	@gen.coroutine		
+	def shutdown(self):
+		logging.info('Stopping http server')
+
+		logging.info('Will shutdown in %s seconds ...', MAX_WAIT_SECONDS_BEFORE_SHUTDOWN)
+		io_loop = tornado.ioloop.IOLoop.instance()
+
+		deadline = time.time() + MAX_WAIT_SECONDS_BEFORE_SHUTDOWN
+
+
+		def stop_loop():
+			now = time.time()
+			if now < deadline and (io_loop._callbacks or io_loop._timeouts):
+				io_loop.add_timeout(now + 1, stop_loop)
+			else:
+				io_loop.stop()
+				logging.info('Shutdown')
+
+		yield stop_loop()
+
+
+
+	def start( self ):
 
 		self.application = tornado.web.Application([
 			(r'/ws', WSHandler),
@@ -132,53 +175,32 @@ class FiscalberryServer:
 		hasKey = self.configberry.config.has_option('SERVIDOR', "ssl_key_path")
 
 
+		self.http_server = tornado.httpserver.HTTPServer(self.application)
+
 		if ( hasCrt and hasKey):
 			ssl_crt_path = self.configberry.config.get('SERVIDOR', "ssl_crt_path")
 			ssl_key_path = self.configberry.config.get('SERVIDOR', "ssl_key_path")
-
 			context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
 			context.load_cert_chain(certfile=ssl_crt_path, keyfile=ssl_key_path)
 
-			self.http_server = tornado.httpserver.HTTPServer(self.application, ssl_options=context)
-			print("iniciando en modo HTTPS")
-		else:
-			self.http_server = tornado.httpserver.HTTPServer(self.application)
-			print("iniciando en modo HTTP")
+			self.https_server = tornado.httpserver.HTTPServer(self.application, ssl_options=context)
+			
 
-
-
-	def shutdown(self):
-		logging.info('Stopping http server')
-
-		logging.info('Will shutdown in %s seconds ...', MAX_WAIT_SECONDS_BEFORE_SHUTDOWN)
-		io_loop = tornado.ioloop.IOLoop.instance()
-
-		deadline = time.time() + MAX_WAIT_SECONDS_BEFORE_SHUTDOWN
-
-		if self.timerPrinterWarnings:
-			self.timerPrinterWarnings.cancel()
-
-		def stop_loop():
-			now = time.time()
-			if now < deadline and (io_loop._callbacks or io_loop._timeouts):
-				io_loop.add_timeout(now + 1, stop_loop)
-			else:
-				io_loop.stop()
-				logging.info('Shutdown')
-
-		stop_loop()
-
-
-
-	def start( self ):
 
 		self.print_printers_resume()
-		puerto = self.get_config_port()
-		self.http_server.bind( puerto )
-		self.http_server.start(0)
-
 		myIP = socket.gethostbyname(socket.gethostname())
-		print '*** Websocket Server Started at %s port %s***' % (myIP, puerto)
+		
+		puerto = self.configberry.config.get('SERVIDOR', "puerto")
+		self.http_server.bind( puerto )
+		self.http_server.start()
+		print '*** Websocket Server Started as HTTP at %s port %s***' % (myIP, puerto)
+
+		if self.https_server:
+			puerto = int(puerto)+1
+			self.https_server.bind( puerto )
+			self.https_server.start()
+			print '*** Websocket Server Started as HTTPS at %s port %s***' % (myIP, puerto )
+
 		# tornado.ioloop.IOLoop.instance().start()
 		tornado.ioloop.IOLoop.current().start()
 
@@ -186,31 +208,11 @@ class FiscalberryServer:
 		logging.info("Exit...")
 			
 
-		
-
-	def get_config_port(self):
-		"lee el puerto configurado por donde escuchara el servidor de websockets"
-		
-		puerto = self.configberry.config.get('SERVIDOR', "puerto")
-		return puerto
-
-
-
-
-	def get_list_of_configured_printers( self ):
-		"Listar las impresoras configuradas"
-		# el primer indice del array corresponde a info del SERVER,
-		# por eso lo omito. El resto son todas impresoras configuradas
-		printers = self.configberry.sections()[1:]
-		return printers
-
-
-
-
 
 
 	def print_printers_resume(self):
-		printers = self.get_list_of_configured_printers()
+		
+		printers = self.configberry.sections()[1:]
 
 		if len(printers) > 1:
 			print "Hay %s impresoras disponibles" % len(printers)
