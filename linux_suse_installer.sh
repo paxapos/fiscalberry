c1a7641ac6bdb637b7b38c1e96f4283ae1b8dc96
paxapos <paxapos@linux-wtyg.suse>
2017-12-09 12:55:09 -0300

    mejora para el installer suse

--- linux_suse_installer.sh
+++ linux_suse_installer.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-zypper install python-pip build-essential python-dev python-imaging python-setuptools libjpeg-dev nmap
+zypper install python-pip build-essential python-dev python-imaging 
+python-setuptools libjpeg-dev nmap
 pip install pyserial
 pip install python-nmap
 pip install requests
 pip install tornado
 pip install python-escpos
 pip install pyutf8
+pip install python-daemon
 
+
+echo "este el el hostname:"
+hostname
+echo "no olvides poner el hostname en el archivo /etc/hosts"
