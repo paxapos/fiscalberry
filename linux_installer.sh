701443951e97a9d33ce1e9afcbeac5fbb14eeea4
alanemiliano <alan.quispe.torrico@gmail.com>
2017-12-28 14:30:15 -0300

    Arreglo en la creación del daemon y el servicio

--- linux_installer.sh
+++ linux_installer.sh
@@ -18,14 +18,21 @@ pip install tornado
 pip install python-escpos
 pip install pyutf8
 
-cp fiscalberry-server-rc /etc/init.d/
-sed "s@WRITEPATHHERE@$(pwd)@" fiscalberry-server-rc > /etc/init.d/fiscalberry-server-rc
-
 echo -n "Crear daemond fiscalberry-server-rc (y/n)? "
 read answer
 if echo "$answer" | grep -iq "^y" ;then
+	cp fiscalberry-server-rc /etc/init.d/
+	sed "s@WRITEPATHHERE@$(pwd)@" fiscalberry-server-rc > /etc/init.d/fiscalberry-server-rc
 	sudo update-rc.d fiscalberry-server-rc defaults
-    echo "Se configuró el daemond"
+  echo "Se configuró el daemond"
 else
-    echo "Finalizó todo correctamente sin instalar el daemond. Deberá iniciarlo manualmente."
-fi
+  echo -n "Instalar el servicio fiscalberry.service (y/n)? "
+  read answer
+  if echo "$answer" | grep -iq "^y" ;then
+    sudo cp fiscalberry.service /etc/systemd/system/
+    cd /etc/systemd/system/
+    sudo systemctl start fiscalberry.service
+    echo "Se inicializo el servicio "
+  else
+    echo "Finalizó todo correctamente sin instalar el daemond ni el servicio. Deberá iniciarlos manualmente."
+  fi
--- linux_suse_installer.sh
+++ linux_suse_installer.sh
@@ -10,6 +10,15 @@ pip install python-escpos
 pip install pyutf8
 pip install python-daemon
 
+echo -n "Instalar el servicio fiscalberry.service (y/n)? "
+read answer
+if echo "$answer" | grep -iq "^y" ;then
+	cp fiscalberry.service /etc/systemd/system/
+	sudo systemctl start fiscalberry.service
+    echo "Se inicializo el servicio "
+else
+    echo "Finalizó todo correctamente sin instalar el servicio. Deberá iniciarlo manualmente."
+fi
 
 echo "este el el hostname:"
 hostname
