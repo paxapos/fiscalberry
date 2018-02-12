f0e6a7011286b8125371341cdd0152a14d89dfb8
gonzaabel <gonzaloabel@hotmail.com>
2018-02-12 20:19:54 -0300

    Generado evento para crear json y guardar los cambios en la configuración, entre otros pequeños cambios

--- js_browser_client/config_json_generator.js
+++ js_browser_client/config_json_generator.js
@@ -1,7 +1,7 @@
 $(document).on("ready", function(){
 
 	$(".panel_configuracion").on("click", function(){
-		let button_string = "Abrir panel de configuración de Fiscalberry";
+		var button_string = "Abrir panel de configuración de Fiscalberry";
 		$(this).val() == button_string ? al_mostrar_panel() : al_cerrar_panel();
 		$("#configPanel").toggle("fade");
 	});
@@ -15,7 +15,7 @@ $(document).on("ready", function(){
 	}
 
 	$("#listadoImpresoras").on("click", ".eliminar_impresora", function(){
-		let confirmacion = confirm("¿Esta seguro que desea eliminar esa impresora del config.ini?");
+		var confirmacion = confirm("¿Esta seguro que desea eliminar esa impresora del config.ini?");
 		if(confirmacion) {
 			$(this).parent().remove();
 		}
@@ -27,27 +27,27 @@ $(document).on("ready", function(){
 	});
 
 	fbrry.on("fb:rta:getActualConfig", function (ob, evt) {
-        let template_for_server = $("#inputsServidor").html();
-        let template_for_printer = $("#nuevaImpresora").html();
+        var template_for_server = $("#inputsServidor").html();
+        var template_for_printer = $("#nuevaImpresora").html();
 		
-        for(let key in evt.data) {
+        for(var key in evt.data) {
         	if(key === "SERVIDOR") {
-        		let puerto = evt.data[key].puerto;
+        		var puerto = evt.data[key].puerto;
         		$("#sectionServidor").append(template_for_server);
         		$("input[name='server_port']").val(puerto);
         		$("#sectionServidor").show();
         	} else {
-        		let nombre_impresora = key;
-        		let marca_impresora = evt.data[key].marca;
-        		let driver_impresora = evt.data[key].driver;
-        		let modelo_impresora = evt.data[key].modelo;
-        		let host_impresora = evt.data[key].host;
-        		let path_impresora = evt.data[key].path;
-        		let codepage_impresora = evt.data[key].codepage;
+        		var nombre_impresora = key;
+        		var marca_impresora = evt.data[key].marca;
+        		var driver_impresora = evt.data[key].driver;
+        		var modelo_impresora = evt.data[key].modelo;
+        		var host_impresora = evt.data[key].host;
+        		var path_impresora = evt.data[key].path;
+        		var codepage_impresora = evt.data[key].codepage;
 
         		$("#listadoImpresoras").append(template_for_printer);
 
-        		let div_impresora = $("#listadoImpresoras > .impresora:last-child");
+        		var div_impresora = $("#listadoImpresoras > .impresora:last-child");
         		
         		$(div_impresora).children("input[name='nombre_impresora']").val(nombre_impresora);
         		$(div_impresora).children("select[name='marca_impresora']").val(marca_impresora).trigger("change");
@@ -65,7 +65,7 @@ $(document).on("ready", function(){
         			$(div_impresora).children("input[name='host_path']").val(path_impresora);
         		}
         		if(codepage_impresora) {
-        			$(div_impresora).children("input[name='codepage_impresora']").val(codepage_impresora);
+        			$(div_impresora).children(".codepage_impresora").children("input[name='codepage_impresora']").val(codepage_impresora);
         		}
         	}
         }
@@ -74,10 +74,13 @@ $(document).on("ready", function(){
     });
 
     $("#listadoImpresoras").on("change", "select[name='marca_impresora']",function(){
-    	if($(this).val() === "Hasar") {
+    	if($(this).val() === "Hasar" || $(this).val() === "Epson") {
+    		//Las fiscales no tienen codepage pero si modelo.
     		$(this).parent().children(".modelo_impresora").show();
+    		$(this).parent().children(".codepage_impresora").hide();
     	} else {
     		$(this).parent().children(".modelo_impresora").hide();
+    		$(this).parent().children(".codepage_impresora").show();
     	}
     });
 
@@ -89,5 +92,62 @@ $(document).on("ready", function(){
     	}
     });
 
+    $("#guardarConfiguracion").on("click", function(){
+    	var jsonConfig = {'configure': {'puerto': '', printerName: "SERVIDOR"}};
+    	var div_configPanel = $(this).parent();
+    	var div_servidor = $(div_configPanel).children("#sectionServidor");
+    	var div_impresoras = $(div_configPanel).children("#listadoImpresoras");
+
+    	var puerto_servidor = $(div_servidor).children("input[name='server_port']").val();
+    	jsonConfig['configure']['puerto'] = puerto_servidor;
+
+    	//primero enviamos para actualizar la sección servidor
+    	jsonConfigString = JSON.stringify(jsonConfig);
+	    fbrry.send(jsonConfigString);
+
+    	for(var key in $(div_impresoras).children(".impresora")) {
+    		var printer = $(div_impresoras).children(".impresora")[key];
+
+    		var nombre_impresora = $(printer).children("input[name='nombre_impresora']").val();
+    		if(typeof nombre_impresora == "undefined") {
+    			break; 
+    			//despues de las impresoras, viene mucha "basura" que hace girar más el for,
+    			//así que hago un break cuando ya el nombre es undefined y listo.
+    		}
+
+    		var marca_impresora = $(printer).children("select[name='marca_impresora']").val();
+
+    		if(marca_impresora === "Hasar" || marca_impresora === "Epson") {
+    			var modelo_impresora = $(printer).children(".modelo_impresora").children("input[name='modelo_impresora']").val();
+    		} else {
+    			var modelo_impresora = "";
+    		}
+
+    		var driver_impresora = $(printer).children("select[name='driver_impresora']").val();
+
+    		var codepage_impresora = $(printer).children(".codepage_impresora").children("input[name='codepage_impresora']").val();
+
+    		var tipo_conexion_impresora = $(printer).children("select[name='tipo_conexion_impresora']").val();
+
+    		var host_path = $(printer).children("input[name='host_path']").val();
+
+    		jsonConfig['configure'] = {};
+    		jsonConfig['configure']['marca'] = marca_impresora;
+    		jsonConfig['configure']['driver'] = driver_impresora;
+    		jsonConfig['configure'][tipo_conexion_impresora] = host_path;
+    		jsonConfig['configure']['printerName'] = nombre_impresora;
+
+    		if(modelo_impresora) {
+    			jsonConfig['configure']['modelo'] = modelo_impresora;
+    		} 
+    		if(codepage_impresora) {
+    			jsonConfig['configure']['codepage'] = codepage_impresora;
+    		}
+    		
+    		//vamos enviando para guardar la configuración de cada printer por separado.
+	    	jsonConfigString = JSON.stringify(jsonConfig);
+	    	fbrry.send(jsonConfigString);
+    	}
+    });
 	
 });
