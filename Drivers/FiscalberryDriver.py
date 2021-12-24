60702e32f7e4113da46c330f94d81a0bd87c1123
Alejandro Vilar <alevilar@gmail.com>
2021-12-24 08:40:05 -0300

    fisclaberry a fiscalberry con posibilidad de cambiar el nombre de la impresora donde imprimir

--- Drivers/FiscalberryDriver.py
+++ Drivers/FiscalberryDriver.py
@@ -20,13 +20,14 @@ class FiscalberryDriver(DriverInterface):
 	printerStatusErrors = []
 
 
-	def __init__(self, host, port=12000, uri = "http", user = None, password = None, after_port = "api"):
+	def __init__(self, host, printerName = "", port=12000, uri = "http", user = None, password = None, after_port = "api"):
 		logging.getLogger().info("conexion con JSON Driver en uri: %s, host: %s puerto: %s" % (uri, host, port))
 		self.host = host
 		self.port = port
 		self.uri = uri
 		self.user = user
 		self.password = password
+		self.printerName = printerName
 		self.after_port = after_port
 		self.url = "%s://%s:%s/%s" % (uri, host, port, after_port)
 
@@ -43,26 +44,24 @@ class FiscalberryDriver(DriverInterface):
 		url = self.url
 
 		logging.getLogger().info("conectando a la URL %s"%url)
-		print jsonData
 		headers = {'Content-type': 'application/json'}
 
+		if self.printerName and jsonData["printerName"]:
+			jsonData["printerName"] = self.printerName
 
 		try: 
 			if self.password:
 				reply = requests.post(url, data=json.dumps(jsonData), headers=headers, auth=(self.user, self.password))
 			else:
 				reply = requests.post(url, data=json.dumps(jsonData), headers=headers)
-			print("INICIANDO::::")
-			print reply
-			print reply.content
-			print("salio la respuesta")
 			print(reply.content)
 			
 			return reply.content
 			
-		except requests.exceptions.Timeout:			
-		    # Maybe set up for a retry, or continue in a retry loop
-		    logging.getLogger().error("timeout de conexion con la impresora fiscal")
+		except requests.exceptions.Timeout as e:			
+			# Maybe set up for a retry, or continue in a retry loop
+			logging.getLogger().error("timeout de conexion con la impresora fiscal")
+		
 		except requests.exceptions.RequestException as e:
 		    # catastrophic error. bail.
 		    logging.getLogger().error(str(e))
