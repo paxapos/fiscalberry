5579e3e6fbb5e20103809515f51f1d094063e9bd
spetrungaro <spetrungaro@hotmail.com>
2022-03-10 10:29:31 -0300

    Eliminados archivos innecesarios

--- probandocurl.sh
+++
@@ -1,4 +0,0 @@
-#bash
-
-#epson tm 700ta 2gen ejemplo (no funciona pero asi me dieron los de epson)
-curl --noproxy 192.168.1.38 http://192.168.1.38/fiscal.json -H "Content-Type: application/json" --data-binary datajson.json > resp.json
