5cd302e0d373743b3089027173968766558ebb16
spetrungaro <spetrungaro@hotmail.com>
2022-03-12 17:40:19 -0300

    logging de ApiRest

--- ApiRest/ApiRestHandler.py
+++ ApiRest/ApiRestHandler.py
@@ -87,8 +87,7 @@ class ApiRestHandler(tornado.web.RequestHandler):
         traductor = self.traductor
         body = self.body
         response = {}
-        logging.info("----- - -- - - - ---")
-        logging.info(body)
+        logging.info(f"Request \n -> {body}")
 
         try:
             response = traductor.json_to_comando(body)
@@ -134,4 +133,5 @@ class ApiRestHandler(tornado.web.RequestHandler):
             import sys, traceback
             traceback.print_exc(file=sys.stdout)
 
+        logging.info("Response \n <- %s" % response)
         self.write(response)
