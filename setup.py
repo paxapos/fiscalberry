a4299bd6b65b3f723ab02c3191adab789427f2a3
Santiago gay <gaysantiago4@gmail.com>
2026-09-08 13:07:14 -0300

    chore(release): bump version 3.6.5 -> 3.6.6

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def load_requirements(filename):
 
 setup(
     name='fiscalberry',
-    version='3.6.5',
+    version='3.6.6',
     description='Proyecto con interfaz Kivy y consola',
     author='Ale Vilar',
     author_email='alevilar@gmail.com',
